04d723992ee0b0c15b369a3d9ede1d7c30e8e843
Majed Ahmed <majid.ahmed-2@student.manchester.ac.uk>
2024-02-09 11:21:58 +0000

    my first commit

---
+++ hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // directive to include 
+
+int main(void) // function which is run when the programs starts
+{
+    printf("Hello Majed"); // our statement/command to run
+
+return 0; // the return statement for the function main
+}
